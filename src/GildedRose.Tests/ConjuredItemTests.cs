2ec4e57a9395e285d0435004d3b1dfb5414b3c16
Nick <npizzolato@gmail.com>
2014-04-03 23:00:20 -0700

    Added test stubs for conjured items

---
+++ src/GildedRose.Tests/ConjuredItemTests.cs
@@ -0,0 +1,29 @@
+﻿namespace GildedRose.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ConjuredItemTests
+    {
+        [Test]
+        public void ConjuredItemsSellinDecreaseByOne()
+        {
+        }
+
+        [Test]
+        public void UnexpiredConjuredItemQualityDecreasesByTwo()
+        {
+        }
+
+        [Test]
+        public void ExpiredConjuredItemQualityDecreasesByFour()
+        {
+        }
+
+        [Test]
+        public void ConjuredItemQualityCannotGoNegative()
+        {
+        }
+    }
+}
