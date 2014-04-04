beb3e2cc2df72e1d8516a2720b486e288d5f31a8
Nick <npizzolato@gmail.com>
2014-04-03 21:22:44 -0700

    Got unit tests executing

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,14 +1,14 @@
-using NUnit.Framework;
-
 namespace GildedRose.Tests
 {
+    using FluentAssertions;
+    using NUnit.Framework;
+
     [TestFixture]
     public class TestAssemblyTests
     {
         [Test]
         public void TestTheTruth()
         {
-            Assert.IsTrue(true);
         }
     }
 }
